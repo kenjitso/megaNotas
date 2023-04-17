2469df209a409e88c27bea2be340c7c613b793b0
Luan Santos Pinto <luan.santos.pinto@gmail.com>
2023-04-17 20:27:13 -0300

    Arrumado atacadogames + adicionado link com o firebase

--- src/functions/lojas/atacadoGames.ts
+++ src/functions/lojas/atacadoGames.ts
@@ -1,37 +1,38 @@
-import { IProdutoLoja } from "@/datatypes/ProdutoLoja";
 import { toast } from "react-toastify";
+
+import { IProdutoLoja } from "@/datatypes/ProdutoLoja";
 import { z } from "zod";
 
 
 export function AtacadoGamesFormat(idLoja: string, excelArray: unknown[]): Array<IProdutoLoja> {
 
 
-  try {
-    excelArray.splice(0, 1);
+    try {
+        excelArray.splice(0, 1);
 
-    const removeUSDollar = (value: string) => value.replace("U$", "").trim();
+        const removeUSDollar = (value: string) => value.replace("U$", "").trim();
 
-    const lineValidation = z.array(
-      z.string().or(z.number().transform(numero => numero.toString())).or(z.null().transform(() => ""))
-    ).min(3).transform(array => ({
-      loja: idLoja,
-      codigo: array[0],
-      nome: array[1],
-      categoria: "",
-      preco: parseFloat(removeUSDollar(array[2])),
-      estoque: true,
-      ultima_atualizacao: new Date(), // .toISOString() exemplo de data válida
-      vinculos: [],
-    }));
+        const lineValidation = z.array(
+            z.string().or(z.number().transform(numero => numero.toString())).or(z.null().transform(() => ""))
+        ).min(3).transform(array => ({
+            id: "",
+            loja: idLoja,
+            codigo: array[0],
+            nome: array[1],
+            categoria: "",
+            preco: parseFloat(removeUSDollar(array[2])),
+            estoque: true,
+            ultima_atualizacao: new Date(), // .toISOString() exemplo de data válida
+            vinculos: [],
+        }));
 
 
+        const itens: IProdutoLoja[] = z.array(lineValidation).parse(excelArray);
 
-    const itens: Array<IProdutoLoja> = z.array(lineValidation).parse(excelArray);
-
-    return itens;
-  } catch (error) {
-    toast.error(`Erro ao processar arquivo: ${error}`);
-    return [];
-  }
+        return itens;
+    } catch (error) {
+        toast.error(`Erro ao processar arquivo: ${error}`);
+        return [];
+    }
 }
 
